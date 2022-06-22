20552f39cfd9ae382e2fab981716485e5c1ced5f
Kirill <89740335+KirillShabanov@users.noreply.github.com>
2022-06-22 19:32:19 +0300

    Add files via upload

---
+++ sendToEmail-HomeWork/sendToEmail-HomeWork/spring-thymeleaf-email2/src/main/java/org/itstep/springthymeleafemail/EmailService.java
@@ -0,0 +1,47 @@
+package org.itstep.springthymeleafemail;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine springTemplateEngine;
+
+    public void sendTextMessage(Email email){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(email.getFrom());
+        message.setTo(email.getTo());
+        message.setSubject(email.getSubject());
+        message.setText(email.getText());
+        javaMailSender.send(message);
+    }
+
+    public void sendHtmlMessage(Email email) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+        Context context = new Context();
+        context.setVariables(email.getProperties());
+        helper.setFrom(email.getFrom());
+        helper.setTo(email.getTo());
+        helper.setSubject(email.getSubject());
+        String html = springTemplateEngine.process(email.getTemplate(),
+                context);
+        helper.setText(html, true);
+        javaMailSender.send(message);
+    }
+}
